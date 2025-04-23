9377ed8dc3cce598ad13eb9f48e31c0b462a5367
EloyR <eloy.roger.araya@gmail.com>
2025-04-23 19:31:33 +0000

    terminado

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SecondsCounter from './components/SecondsCounter';
+import Buttons from './components/Buttons';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let counter = 0;
+let intervalId;
+let targetAlert = null;
+function renderApp() {
+  root.render(
+    <React.StrictMode>
+      <SecondsCounter seconds={counter} />
+      <Buttons
+        onStop={() => clearInterval(intervalId)}
+        onReset={() => {
+          counter = 0;
+          renderApp();
+        }}
+        onSetAlert={(seconds) => {
+          targetAlert = seconds;
+        }}
+      />
+    </React.StrictMode>
+  );
+  // Verificar alerta
+  if (targetAlert !== null && counter === targetAlert) {
+    alert(`¡Alerta! Llegaste a ${targetAlert} segundos`);
+    targetAlert = null;
+  }
+}
+intervalId = setInterval(() => {
+  counter += 1;
+  renderApp();
+}, 1000);
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
-
-// index.css'
-import '../styles/index.css'
-
-// components
-import Home from './components/Home';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+renderApp();
